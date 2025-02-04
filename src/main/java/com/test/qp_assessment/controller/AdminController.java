0fb5bea0fc681ad89b2ee2c2d5b81e85f3b4bc6e
Yash Garala <yashgarala29@gmail.com>
2025-02-04 16:52:48 +0530

    admin functionality added

--- src/main/java/com/test/qp_assessment/controller/AdminController.java
+++ src/main/java/com/test/qp_assessment/controller/AdminController.java
@@ -1,13 +1,13 @@
 package com.test.qp_assessment.controller;
 
+import com.test.qp_assessment.dto.ProductDetails;
 import com.test.qp_assessment.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin")
@@ -16,9 +16,24 @@ public class AdminController {
     private AdminService adminService;
 
     @PostMapping("/addProduct")
-    public ResponseEntity<String> addProduct() {
-        adminService.addProduct();
-        return new ResponseEntity<>("Product added successfully", HttpStatus.OK);
+    public ResponseEntity<String> addProduct(@RequestBody ProductDetails productDetails) {
+        return adminService.addProduct(productDetails);
+    }
+    @GetMapping("/getAllProducts")
+    public ResponseEntity<List<ProductDetails>> getAllProducts() {
+        return adminService.getAllProducts();
+    }
+    @DeleteMapping("/deleteProduct/{productId}")
+    public ResponseEntity<String> deleteProduct(@PathVariable("productId") Long productId) {
+        return adminService.deleteProduct(productId);
+    }
+    @PatchMapping("/updateProduct")
+    public ResponseEntity<String> updateProduct(@RequestBody ProductDetails productDetails) {
+        return adminService.updateProduct(productDetails);
+    }
+    @PatchMapping("/updateProductQuantity")
+    public ResponseEntity<String> updateProductQuantity(@RequestBody ProductDetails productDetails) {
+        return adminService.updateProductQuantity(productDetails);
     }
 
 
--- src/main/java/com/test/qp_assessment/service/AdminService.java
+++ src/main/java/com/test/qp_assessment/service/AdminService.java
@@ -1,13 +1,73 @@
 package com.test.qp_assessment.service;
 
 import com.test.qp_assessment.dto.ProductDetails;
+import com.test.qp_assessment.model.Grocery;
+import com.test.qp_assessment.repository.GroceryRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AdminService {
-    public void addProduct() {
-        ProductDetails productDetails=new ProductDetails();
-        productDetails.setItemName("Milk");
-        System.out.println("Product added successfully"+productDetails.getItemName());
+    @Autowired
+    private GroceryRepo groceryRepo;
+    public ResponseEntity<String> addProduct(ProductDetails productDetails) {
+            Grocery grocery=new Grocery();
+            grocery.setPrice(productDetails.getPrice());
+            grocery.setQuantity(productDetails.getQuantity());
+            grocery.setItemName(productDetails.getItemName());
+            groceryRepo.save(grocery);
+            return new ResponseEntity<>("Product added successfully", HttpStatus.OK);
+        }
+
+    public ResponseEntity<List<ProductDetails>> getAllProducts() {
+        List<Grocery> groceryList=groceryRepo.findAll();
+        List<ProductDetails> productDetailsList= groceryList.stream().map(grocery -> {
+            ProductDetails productDetails=new ProductDetails();
+            productDetails.setId(grocery.getId());
+            productDetails.setItemName(grocery.getItemName());
+            productDetails.setPrice(grocery.getPrice());
+            productDetails.setQuantity(grocery.getQuantity());
+            return productDetails;
+        }).toList();
+        return new ResponseEntity<>(productDetailsList,HttpStatus.OK);
+    }
+
+    public ResponseEntity<String> deleteProduct(Long productId) {
+        groceryRepo.deleteById(productId);
+        return new ResponseEntity<>("Product deleted successfully",HttpStatus.OK);
+    }
+
+    public ResponseEntity<String> updateProduct(ProductDetails productDetails) {
+        Grocery grocery=groceryRepo.findById(productDetails.getId()).orElse(null);
+        if(grocery==null){
+            return new ResponseEntity<>("Product not found",HttpStatus.NOT_FOUND);
+        }
+        if(productDetails.getItemName()!=null){
+            grocery.setItemName(productDetails.getItemName());
+        }
+        if(productDetails.getPrice()!=null){
+            grocery.setPrice(productDetails.getPrice());
+        }
+        if(productDetails.getQuantity()!=null){
+            grocery.setQuantity(productDetails.getQuantity());
+        }
+        groceryRepo.save(grocery);
+        return new ResponseEntity<>("Product updated successfully",HttpStatus.OK);
+    }
+
+    public ResponseEntity<String> updateProductQuantity(ProductDetails productDetails) {
+        Grocery grocery=groceryRepo.findById(productDetails.getId()).orElse(null);
+        if(grocery==null){
+            return new ResponseEntity<>("Product not found",HttpStatus.NOT_FOUND);
+        }
+        if(productDetails.getQuantity()!=null){
+            grocery.setQuantity(productDetails.getQuantity());
+        }
+        groceryRepo.save(grocery);
+        return new ResponseEntity<>("Product quantity updated successfully",HttpStatus.OK);
     }
 }
