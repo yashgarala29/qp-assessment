579a2c1dec2e708e6aca973bc0e0f22f2dacb48f
Yash Garala <yashgarala29@gmail.com>
2025-02-04 15:28:21 +0530

    init

---
+++ src/main/java/com/test/qp_assessment/QpAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.test.qp_assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QpAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QpAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/qp_assessment/QpAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.qp_assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QpAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
